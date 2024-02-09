a284e3c0477a129e2938d4c62a2f8df19675b25b
Cliff <461878+cliffcole@users.noreply.github.com>
2024-02-08 23:59:56 -0500

    editing git?

--- install/install.sh
+++ install/install.sh
@@ -2,7 +2,8 @@
 
 sudo yum check-update
 sudo yum update -y
-sudo yum install git httpd -y
+sudo yum install git -y
+sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo cp /tmp/html/index.html /var/www/html
